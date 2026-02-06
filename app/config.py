c57d150af4dfaff30e3072ad760170692932443b
pgrapher906-ctrl <pgrapher906@gmail.com>
2026-02-06 19:55:29 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'change_this_secret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET_KEY = os.getenv("SECRET_KEY", "change_this_secret")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
